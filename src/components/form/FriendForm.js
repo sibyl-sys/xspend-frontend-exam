9a49f77b2b6af042c483428aeca5703f6411ff82
mcanjelo <mcanjelo29@gmail.com>
2021-12-16 21:51:10 +0800

    Add Form Input Handlers

--- src/components/form/FriendForm.js
+++ src/components/form/FriendForm.js
@@ -1,20 +1,50 @@
 import React, {useContext, useState} from "react";
 import { Container, Form, FormGroup, Input, Col, Row, Button } from "reactstrap"
 
+
+
 export default function FriendForm () {
+
+    const [name, setName] = useState("");
+    const [wallet, setWallet] = useState("");
+    const [email, setEmail] = useState("");
+    const [errors, setErrors] = useState({});
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    function handleNameChange(event) {
+        setName(event.target.value);
+    } 
+    
+    function handleWalletChange(event) {
+        setWallet(event.target.value);
+    }
+
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
+
+
+
+
     return <Container 
         className="mt-5"
     >
         <Row className="justify-content-md-center  ">
             <Col sm={6} className="bg-light border p-3">
                 FORM 
-                <Form className="mt-2">
+                <Form className="mt-2" onSubmit={handleSubmit}>
                     <FormGroup row>
                         <Col>
                             <Input
                                 id="name"
                                 name="name"
-                                placeHolder="Name"
+                                placeholder="Name"
+                                value={name}
+                                onChange={handleNameChange}
                             />
                         </Col>
                     </FormGroup>
@@ -23,7 +53,9 @@ export default function FriendForm () {
                             <Input
                                 id="wallet"
                                 name="wallet"
-                                placeHolder="Wallet Address"
+                                placeholder="Wallet Address"
+                                value={wallet}
+                                onChange={handleWalletChange}
                             />
                         </Col>
                     </FormGroup>
@@ -32,8 +64,10 @@ export default function FriendForm () {
                             <Input
                                 id="email"
                                 name="email"
-                                placeHolder="Email"
+                                placeholder="Email"
                                 type="email"
+                                value={email}
+                                onChange={handleEmailChange}
                             />
                         </Col>
                     </FormGroup>
