6bee32366561f55291f9e5c76253a569203accf6
mcanjelo <mcanjelo29@gmail.com>
2021-12-16 20:57:07 +0800

    Add friends list provider.

---
+++ src/components/providers/FriendsList.js
@@ -0,0 +1,65 @@
+import React, {useContext, useState} from "react";
+
+const FriendsListContext = React.createContext([]);
+const FriendsListAddContext = React.createContext(friend => () => {
+    console.log("Add " + friend.name + " to friendslist!")
+});
+const FriendsListUpdateContext = React.createContext((index, friend) => () => {
+    console.log("Updated index " + index + " of friendslist!")
+});
+const FriendDeleteContext = React.createContext((index) => () => {
+    console.log("Friend at index " + index + " deleted!")
+});
+
+export function useFriendsList() {
+    return useContext(FriendsListContext);
+}
+
+export function useFriendsListAdd() {
+    return useContext(FriendsListAddContext);
+}
+
+export function useFriendsListUpdate() {
+    return useContext(FriendsListUpdateContext);
+}
+
+export function useFriendsListDelete() {
+    return useContext(FriendDeleteContext);
+}
+
+export function FriendsListProvider ({children}) {
+    const [friendsList, setFriendsList] = useState(
+        JSON.parse(window.localStorage.getItem("ETHFriendsList")) || []
+    );
+
+    const addFriend = friend => () => {
+        setFriendsList([...friendsList, friend]);
+        window.localStorage.setItem("ETHFriendsList", JSON.stringify(friendsList));
+    } 
+
+    const deleteFriend = (index, friend) => () => {
+        const updatedFriendsList = [...friendsList];
+        updatedFriendsList.splice(index, 1);
+        setFriendsList(updatedFriendsList);
+        window.localStorage.setItem("ETHFriendsList", JSON.stringify(friendsList));
+    } 
+    
+    const updateFriend = (index, friend) => () => {
+        const updatedFriendsList = [...friendsList];
+        updatedFriendsList[index] = friend;
+        setFriendsList(updatedFriendsList);
+        window.localStorage.setItem("ETHFriendsList", JSON.stringify(friendsList));
+    }
+
+    return (
+        <FriendsListContext.Provider value={friendsList}>
+            <FriendsListAddContext.Provider value={addFriend}>
+                <FriendsListUpdateContext.Provider value={deleteFriend}>
+                    <FriendDeleteContext.Provider value={updateFriend}>
+                        {children}
+                    </FriendDeleteContext.Provider>
+                </FriendsListUpdateContext.Provider>
+            </FriendsListAddContext.Provider>
+        </FriendsListContext.Provider>
+    )
+}
