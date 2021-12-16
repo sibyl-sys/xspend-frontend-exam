5fbf2c2c740d90f88bc0817c45113124f7dd58b9
mcanjelo <mcanjelo29@gmail.com>
2021-12-16 23:34:56 +0800

    Fix card layout

--- src/components/friendsList/FriendsListView.js
+++ src/components/friendsList/FriendsListView.js
@@ -1,12 +1,20 @@
 import React, {useState} from "react";
+import { Col, Container, Row } from "reactstrap";
 import FriendsListCard from "./FriendsListCard";
 
 export default function FriendsListView() {
     return <div>
         FRIENDS
-
-        <FriendsListCard />
-        <FriendsListCard />
-        <FriendsListCard />
+        <Row>
+            <Col xs={6}>
+                <FriendsListCard />
+            </Col>
+            <Col xs={6}>
+                <FriendsListCard />
+            </Col>
+            <Col xs={6}>
+                <FriendsListCard />
+            </Col>
+        </Row>
     </div>
 }
